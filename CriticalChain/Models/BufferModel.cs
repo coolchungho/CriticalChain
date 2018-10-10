63ecfec5ba0527b419eb3f1e4637901685caf1d2
coolchungho <coolchungho@gmail.com>
2018-10-10 15:00:50 +0800

    add task & buffer model

---
+++ CriticalChain/Models/BufferModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CriticalChain.Models
+{
+    public class BufferModel
+    {
+        public Int64 Id { get; set; }
+        public int BufferId { get; set; }
+        public string BufferType { get; set; }
+        public int BufferLength { get; set; }
+        public int BufferHeadTaskId { get; set; }
+        public int BufferTailTaskId { get; set; }
+    }
+}
---
+++ CriticalChain/Models/TaskModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CriticalChain.Models
+{
+    public class TaskModel
+    {
+        public Int64 Id { get; set; }
+        public int TaskId { get; set; }
+        public bool IsStartTask { get; set; }
+        public bool IsEndTask { get; set; }
+        public string TaskContent { get; set; }
+        public string TaskOwner { get; set; }
+        public DateTime PlannedStartDate { get; set; }
+        public DateTime PlannedEndDate { get; set; }
+        public int PlannedLength { get; set; }
+        public DateTime ActualStartDate { get; set; }
+        public DateTime ActualEndDate { get; set; }
+        public int ActualLength { get; set; }
+        public int PreLinkedTaskId { get; set; }
+        public int PostLinkedTaskId { get; set; }
+        public string Status { get; set; }
+    }
+}

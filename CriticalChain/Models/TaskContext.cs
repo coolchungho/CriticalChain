5b53c10775cb242b9df94d2c690cfaa9eb4015b3
謝忠和 <coolchungho@gmail.com>
2018-10-13 11:33:34 +0800

    add DbContext

---
+++ CriticalChain/Models/TaskContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CriticalChain.Models;
+
+namespace CriticalChain.Models
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+
+        }
+            public DbSet<TaskModel> Tasks { get; set; }
+    }
+}
